e3ffe5e0cfe998e35be4ef22cb77d659adf8bb18
SooOverpowered <lehoangminh15122000@gmail.com>
2024-03-08 08:02:26 +0700

    catpuccin everywhere

--- config/nvim/lua/plugins/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "echasnovski/mini.misc",
-    -- config = function()
-    --   require("mini.misc").setup()
-    --   require("mini.misc").setup_auto_root(nil)
-    -- end,
-  },
-}
