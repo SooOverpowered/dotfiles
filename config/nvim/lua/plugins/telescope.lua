87dd83f8ca96bae3f7f280822b80e5f3fd0734f5
lucas <minh.le@ataccama.com>
2024-03-15 23:31:15 +0700

    better telescope symbol search

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -27,6 +27,31 @@ return {
         end,
         desc = "Goto Symbol",
       },
+      {
+        "<leader>sS",
+        function()
+          require("telescope.builtin").lsp_dynamic_workspace_symbols({
+            symbols = {
+              "method",
+              "field",
+              "type",
+              "variable",
+              "class",
+              "interface",
+              "module",
+              "property",
+              "enum",
+              "struct",
+              "event",
+              "operator",
+              "typeParameter",
+              "constant",
+              "function",
+            },
+          })
+        end,
+        desc = "Goto Symbol (Workspace)",
+      },
     },
   },
 }
