24db41f8cea741962f324a747b40789a3e95c8e2
lucas <minh.le@ataccama.com>
2024-03-15 23:30:59 +0700

    better lsp hover

---
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,21 @@
+return {
+  -- LSP keymaps
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      keys[#keys + 1] = { "K", "<cmd>Lspsaga hover_doc<cr>" }
+    end,
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("lspsaga").setup({})
+    end,
+  },
+}
