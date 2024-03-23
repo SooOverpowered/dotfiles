1db7a92142c83f95a523108230489a254ae37f98
SooOverpowered <lehoangminh15122000@gmail.com>
2024-03-23 14:19:40 +0700

    add autosave debounce to 5s

--- config/nvim/lua/plugins/autosave.lua
+++ config/nvim/lua/plugins/autosave.lua
@@ -6,6 +6,10 @@ return {
     opts = {
       -- your config goes here
       -- or just leave it empty :)
+      debounce_delay = 5000,
+    },
+    keys = {
+      { "<leader>ua", ":ASToggle<CR>", desc = "Toggle auto-save" },
     },
   },
 }
