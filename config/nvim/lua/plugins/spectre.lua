dcf767df7e45df72521fe4dff92ecef79e6b3bf8
lucas <minh.le@ataccama.com>
2024-05-27 18:40:47 +0700

    formatting

--- config/nvim/lua/plugins/spectre.lua
+++ config/nvim/lua/plugins/spectre.lua
@@ -4,9 +4,14 @@ return {
     build = false,
     cmd = "Spectre",
     opts = { open_cmd = "noswapfile vnew" },
-    -- stylua: ignore
     keys = {
-      { "<leader>sr", function() require("spectre").open({cwd = vim.loop.cwd(), is_insert_mode = true}) end, desc = "Replace in files (Spectre)" },
+      {
+        "<leader>sr",
+        function()
+          require("spectre").open({ cwd = vim.loop.cwd(), is_insert_mode = true })
+        end,
+        desc = "Replace in files (Spectre)",
+      },
     },
   },
 }
