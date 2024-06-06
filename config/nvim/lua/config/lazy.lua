f3db5342d45036266875dbee5d75cd77a35f6e87
lucas <minh.le@ataccama.com>
2024-06-07 01:57:50 +0700

    add nvim git extras

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -23,6 +23,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.ansible" },
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.helm" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
