549ab233acfde1c9c0dcbcf2824801d831558964
SooOverpowered <minh.le@ofti.app>
2024-11-25 18:32:05 +0000

    fix clipboard in ssh session

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -8,4 +8,5 @@ vim.g.tmux_navigator_preserve_zoom = 1
 vim.g.tmux_navigator_no_wrap = 1
 vim.lsp.set_log_level("off")
 vim.g.lazyvim_picker = "fzf"
+vim.opt.clipboard = "unnamedplus"
 -- vim.g.lazygit_config = false
