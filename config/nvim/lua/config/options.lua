7bf506a1fecf14e2c8b688de827a18fe5d474756
lucas <minh.le@ataccama.com>
2025-05-13 13:07:58 +0700

    cleanup neovim plugins

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -7,5 +7,5 @@ vim.opt.swapfile = false
 vim.g.tmux_navigator_preserve_zoom = 1
 vim.g.tmux_navigator_no_wrap = 1
 vim.lsp.set_log_level("off")
-vim.g.lazyvim_picker = "fzf"
+vim.g.lazyvim_picker = "snacks"
 vim.opt.clipboard = "unnamedplus"
